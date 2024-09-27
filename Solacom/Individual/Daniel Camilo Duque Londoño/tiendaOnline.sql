1fd73648249f7c76551ca09030a2d97f457e05eb
Daniel Duque <148986240+Danielduque22@users.noreply.github.com>
2024-09-27 08:53:37 -0500

    Add files via upload

--- Solacom/Individual/Daniel Camilo Duque Londoño/tiendaOnline.sql
+++ Solacom/Individual/Daniel Camilo Duque Londoño/tiendaOnline.sql
@@ -7,7 +7,7 @@ use TiendaOnline;
 CREATE TABLE Producto(
 codigoBarras INT(15) auto_increment PRIMARY KEY,
 nombreProducto VARCHAR(15),
-precioProducto INT(11)
+precioProducto float
 );
 
 CREATE TABLE Usuario(
@@ -67,7 +67,7 @@ insert into Usuario values(1,'Maria', 'Maria.1@gmail.com','Empleado',1234),(2,'S
 select * from Usuario;
 
 describe Ventas;
-insert into Ventas values(1,'03', 4,2),(2,'05', 4,3), (3,'07', 3,1), (4,'03', 2,3);
+insert into Ventas values(1,'03', 4,2),(2,'05', 4,4), (3,'07', 3,1), (4,'03', 2,3);
 select * from Ventas;
 
 describe Producto;
@@ -75,7 +75,7 @@ insert into Producto values(112,'Audifonos', 42.000),(113,'Labial', 14.300), (11
 select * from Producto;
 
 describe Ventas_Producto;
-insert into Ventas_Producto values(112,3,42.000,2),(113,2,14.300, 3), (114,1,31.000, 1), (115,2,230.000, 3);
+insert into Ventas_Producto values(112,3,42.000,2),(113,2,14.300, 3), (114,1,31.000, 1), (115,2,230.000, 4);
 select * from Ventas_Producto;
 
 select * from Producto;
@@ -89,23 +89,19 @@ select avg(idVentas) from Ventas;
 select sum(idVentas) from Ventas;
 
 use tiendaOnline;
+describe Ventas_Producto;
+describe Ventas;
 select * from Ventas inner join Cliente on Ventas.idClienteFK = Cliente.idCliente;
 -- consultar datos cliente de la maxima venta hecha
-select Cliente.nombreCliente, Ventas.idClienteFK, Ventas_Producto.Cantidad from Ventas
+select Cliente.nombreCliente, Ventas.idClienteFK, Ventas_Producto.Cantidad, max(cantidad) from Ventas
 inner join Cliente on Cliente.idCliente = Ventas.idClienteFK
-inner join Ventas_Producto on Ventas.idVentas = Ventas_Producto.codigoBarrasFK
-where totalVentas = max(totalVentas);
+inner join Ventas_Producto on Ventas.idVentas = Ventas_Producto.codigoBarrasFK;
 -- consultar usuario y cliente de una venta especifica
-
-UPDATE Ventas
-SET idUsuarioFK = 4
-WHERE idVentas =2 ;
 
 SELECT V.idVentas, C.nombreCliente, U.nombreUsuario
 FROM Ventas V
 JOIN Cliente C ON V.idClienteFK = C.idCliente
-JOIN Usuario U ON V.idUsuarioFK = U.idUsuario
-WHERE V.idVentas = 3;
+JOIN Producto P ON V.idUsuarioFK = P.nombreProducto;
 -- consultar los productos que compró un cliente especifico
 -- consultar todos los clientes que han hecho ventas
 describe Ventas_Producto;
@@ -114,3 +110,29 @@ select * from Ventas;
 select * from Cliente;
 select * from Ventas_Producto;
 select * from Producto;
+
+alter table Cliente
+add column apellido varchar(17);
+
+alter table Ventas
+add column codigoBarrasFK int;
+
+alter table Ventas
+add column numeroOrden int not null;
+
+insert into Cliente values(5, 'Tatiana', 2000-06-06);
+insert into Producto values(116, 'pañales',20000);
+insert into Usuario values(5,'Daniel', 'daniel@gmail.com','empleado',9564);
+insert into Ventas values(5,'27', 5,5);
+describe Cliente;
+update Ventas set codigoBarrasFK = 116 where idClienteFK = 5;
+update Cliente set  apellido = 'Cabrera Vargas', fechaNacimiento = '1985-11-24' where idCliente = 5;
+update Ventas set idVentas = 20240927 where idVentas = 5;
+describe Ventas;
+update Ventas set numeroOrden = 45528536 where idClienteFK = 5;
+update Ventas set idVentas = 5 where idClienteFK = 5;
+update ventas set numeroOrden = 20240927 where idClienteFK = 5;
+
+insert into Cliente values(55163118, 'Tatiana','1985-11-24','Cabrera Vargas');
+update Ventas set idClienteFK = 55163118 where idClienteFK = 5;
+delete from Cliente where idCliente = 5;
